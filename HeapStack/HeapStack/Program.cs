804ab470a01bf8227947ac9e91d31a80c02700ea
SadiqNahmetov <sadigfn@code.edu.az>
2022-06-21 21:12:40 +0400

    Homework Reverse

---
+++ HeapStack/HeapStack/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HeapStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Gulperi";
+            string count = ReverseWords(name);
+            Console.WriteLine(count);
+                                  
+        }
+
+
+        static string ReverseWords(string name)
+        {
+            string count = "";
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                count += name[i];
+            }
+            return count;
+
+
+        }
+
+
+    }
+}
